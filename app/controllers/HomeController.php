dbfff82854efd68fcc02d8a31879b10a515565f9
quentin.picart <quentin.picart@gmail.com>
2019-08-17 15:31:04 +0200

    Feat: create home controller

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController
+{   
+    public function __construct()
+    {
+        // TO DO: GET SOME ARTICLES
+        // TO DO: DISPLAY HOME VIEW
+    }
+}
